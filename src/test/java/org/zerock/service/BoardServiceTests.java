b9b2da6cf8e0de91cbb6963422db48901803ce8e
sskim91 <tjdtjq91@gmail.com>
2020-10-11 17:45:18 +0900

    feat: 비즈니스 계층 구현과 테스트

---
+++ src/test/java/org/zerock/service/BoardServiceTests.java
@@ -0,0 +1,67 @@
+package org.zerock.service;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {org.zerock.config.RootConfig.class})
+@Log4j
+public class BoardServiceTests {
+
+    @Setter(onMethod_ = {@Autowired})
+    private BoardService boardService;
+
+    @Test
+    public void testExist() {
+        log.info(boardService);
+        assertNotNull(boardService);
+    }
+
+    @Test
+    public void testRegister() {
+        BoardVO boardVO = new BoardVO();
+        boardVO.setTitle("새로 작성하는 글");
+        boardVO.setContent("새로 작성하는 내용");
+        boardVO.setWriter("newbie");
+
+        boardService.register(boardVO);
+
+        log.info("생성된 게시물의 번호: " + boardVO.getBno());
+    }
+
+    @Test
+    public void testGetList() {
+        boardService.getList().forEach(board -> log.info(board));
+    }
+
+    @Test
+    public void testGet() {
+        log.info(boardService.get(1L));
+    }
+
+    @Test
+    public void testDelete() {
+        //게시물의 번호의 존재 여부를 확인하고 테스트 할 것
+        log.info("REMOVE RESULT: "+ boardService.remove(2L));
+    }
+
+    @Test
+    public void testUpdate() {
+        BoardVO boardVO = boardService.get(1L);
+
+        if (boardVO == null) {
+            return;
+        }
+
+        boardVO.setTitle("제목을 수정합니다.");
+        log.info("MODIFY RESULT: "+ boardService.modify(boardVO));
+    }
+}
