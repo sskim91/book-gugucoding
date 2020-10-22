e6491d98a433ca4e41ba212e61dfd4fcc5fd2dd3
sskim <tjdtjq91@gmail.com>
2020-10-22 17:41:49 +0900

    feat: 파트3 검색 마무리

--- src/main/java/org/zerock/domain/Criteria.java
+++ src/main/java/org/zerock/domain/Criteria.java
@@ -3,6 +3,7 @@ package org.zerock.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.web.util.UriComponentsBuilder;
 
 
 @ToString
@@ -10,23 +11,34 @@ import lombok.ToString;
 @Getter
 public class Criteria {
 
-  private int pageNum;
-  private int amount;
-  
-  private String type;
-  private String keyword;
+    private int pageNum;
+    private int amount;
 
-  public Criteria() {
-    this(1, 10);
-  }
+    private String type;
+    private String keyword;
 
-  public Criteria(int pageNum, int amount) {
-    this.pageNum = pageNum;
-    this.amount = amount;
-  }
-  
-  public String[] getTypeArr() {
-    
-    return type == null? new String[] {}: type.split("");
-  }
+    public Criteria() {
+        this(1, 10);
+    }
+
+    public Criteria(int pageNum, int amount) {
+        this.pageNum = pageNum;
+        this.amount = amount;
+    }
+
+    public String[] getTypeArr() {
+
+        return type == null ? new String[]{} : type.split("");
+    }
+
+    public String getListLink() {
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromPath("")
+                .queryParam("pageNum", this.pageNum)
+                .queryParam("amount", this.amount)
+                .queryParam("type", this.type)
+                .queryParam("keyword", this.keyword);
+
+        return builder.toUriString();
+    }
 }
