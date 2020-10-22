98f7932efc2b0b21e7dabc017e64143c02f3a24f
sskim <tjdtjq91@gmail.com>
2020-10-22 18:06:58 +0900

    feat: RestController 반환타입 기본문자열, 객체, 컬렉션, ResponseEntity 타입

---
+++ src/main/java/org/zerock/domain/SampleVO.java
@@ -0,0 +1,18 @@
+package org.zerock.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author sskim
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SampleVO {
+
+    private Integer mno;
+    private String firstName;
+    private String lastName;
+}
